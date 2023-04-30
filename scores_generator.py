f7faaca09aabfd803e99f1221766e1abb5441625
Mehroos Ali <mehroosali@gmail.com>
2023-04-30 12:01:53 -0500

    update score generation script

--- scores_generator.py
+++ scores_generator.py
@@ -1,51 +1,82 @@
 import json
 
-#page_rank_dict = {}
+page_rank_dict = {}
 rm_scores = {}
 authority_scores = {}
 hubs_scores = {}
-# with open('page_rank_scores.txt', 'r') as file:
-#     for line in file:
-#         line_arr = line.split('\t')
-#         url, score = line_arr[0], float(line_arr[1].strip())
-#         page_rank_dict[url] = score
-    
-with open('rm_scores.txt', 'r') as file:
-    rm_scores = json.load(file)
-    
-with open('authorities_scores.txt', 'r') as file:
-    authority_scores = json.load(file)    
-
-with open('hub_scores.txt', 'r') as file:
-    hubs_scores = json.load(file) 
-
 new_scores = {}
-# for url, score in rm_scores.items():
-#     pr_score = page_rank_dict.get(url, 0)
-#     if score == 0 and pr_score == 0:
-#         new_scores[url] = 0
-#     elif score == 0 and pr_score != 0:
-#         new_scores[url] = pr_score
-#     elif score != 0 and pr_score == 0:
-#         new_scores[url] = score
-#     else: 
-#         new_scores[url] = score*pr_score
+
+def generate_modified_pr_scores():
+    with open('results/page_rank_scores.txt', 'r') as file:
+        for line in file:
+            line_arr = line.split('\t')
+            url, score = line_arr[0], float(line_arr[1].strip())
+            page_rank_dict[url] = score
+    
+    with open('results/rm_scores.txt', 'r') as file:
+        rm_scores = json.load(file)
+
+    for url, score in rm_scores.items():
+        pr_score = page_rank_dict.get(url, 0)
+        if score == 0 and pr_score == 0:
+            new_scores[url] = 0
+        elif score == 0 and pr_score != 0:
+            new_scores[url] = pr_score
+        elif score != 0 and pr_score == 0:
+            new_scores[url] = score
+        else: 
+            new_scores[url] = score*pr_score
+
+    print('Writing new PR scores..')    
+    with open('results/pr_modified_scores.txt', 'w') as convert_file:
+        convert_file.write(json.dumps(new_scores))    
+
+def generate_hits_score():
+
+    with open('results/rm_scores.txt', 'r') as file:
+        rm_scores = json.load(file)
+
+    with open('results/authorities_scores.txt', 'r') as file:
+        authority_scores = json.load(file)    
+
+    with open('results/hub_scores.txt', 'r') as file:
+        hubs_scores = json.load(file) 
   
-for url, score in rm_scores.items():
-    auth_score = authority_scores.get(url, 0)
-    hubs_score = hubs_scores.get(url, 0)
-    
-    if score == 0:
-        new_scores[url] = auth_score + hubs_score
-    else: 
-        new_scores[url] = score * (auth_score + hubs_score)
-    
-print(len(new_scores))
+    for url, score in rm_scores.items():
+        auth_score = authority_scores.get(url, 0)
+        hubs_score = hubs_scores.get(url, 0)
+        
+        if score == 0:
+            new_scores[url] = auth_score + hubs_score
+        else: 
+            new_scores[url] = score * (auth_score + hubs_score)
+        
+    print(len(new_scores))
 
-# print('Writing new PR scores..')    
-# with open('pr_modified_scores.txt', 'w') as convert_file:
-#     convert_file.write(json.dumps(new_scores))
+    print('Writing new HITS scores..')    
+    with open('results/hits_scores.txt', 'w') as convert_file:
+        convert_file.write(json.dumps(new_scores))
 
-print('Writing new HITS scores..')    
-with open('hits_scores.txt', 'w') as convert_file:
-    convert_file.write(json.dumps(new_scores))
+
+def generate_vs_rm_scores_from_solr():
+    with open('results/solr_data.json', encoding="utf8") as f:
+        print("Loading Json file ....")
+        data = json.load(f)
+        print("Loaded successfully!")
+        documents = data['response']['docs']
+        print(f"Length of the documents: {len(documents)}")
+        
+        for record in documents:
+            if 'url' in record:
+                url = record['url']
+                score = record.get('boost', 0)
+                rm_scores[url] = score
+                
+    with open('results/rm_scores.txt', 'w') as convert_file:
+        convert_file.write(json.dumps(rm_scores))
+    print(len(rm_scores))
+
+if __name__ == "__main__":
+    generate_vs_rm_scores_from_solr()
+    generate_modified_pr_scores()
+    generate_hits_score()
